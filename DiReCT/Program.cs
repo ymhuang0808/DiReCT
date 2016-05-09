6436034aa51d53abb1be131c6fef848000198ab6
Jeff Chen <jeff@iis.sinica.edu.tw>
2016-05-09 12:42:52 +0800

    Update Program.cs

--- DiReCT/Program.cs
+++ DiReCT/Program.cs
@@ -12,12 +12,10 @@ namespace DiReCT
 			Fire fireRecord = new Fire ("TaipeiStationFire", "Jim", now);
 			Flood floodRecord = new Flood ("PuliFlood", "JohnsonS", now);
 			Landside landsideRecord = new Landside ("ShiaoLinlandside", "JohnsonH", now);
-
+			
 			Console.WriteLine ("Call fire.UID    => {0}", fireRecord.UID);
 			Console.WriteLine ("Call flood.UID   => {0}", floodRecord.UID);
 			Console.WriteLine ("Call lanside.UID => {0}", landsideRecord.UID);
-            Console.ReadLine();
-
 		}
 
 		public static String GetTimestamp (DateTime value)
